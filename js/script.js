185094a111064d91e60205d0dae7177934d84504
SubhojeetDas1107 <hbksubhojeetdas1107@gmail.com>
2022-05-01 23:08:25 +0530

    Frontend Landing page completed

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+let scrollTopBtn = document.getElementById("scroll-top");
+
+/* SideMenu Toggle */
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+let header = document.querySelector("header");
+window.onscroll = () => {
+  /* Sticky Header */
+  let pos = document.documentElement.scrollTop;
+  if (pos > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+  /* Scroll Top Button */
+  if (pos > 300) {
+    scrollTopBtn.style.display = "grid";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+
+  scrollTopBtn.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
